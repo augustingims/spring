0c5ee1cf66222ba5fafbc67c343de64652d9a1c0
augustingims <augustingims@gmail.com>
2018-08-09 04:11:06 +0100

    update

---
+++ SpringLdapThymleaf/src/main/java/com/teamdev/app/SpringLdapThymleafApplication.java
@@ -0,0 +1,12 @@
+package com.teamdev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLdapThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLdapThymleafApplication.class, args);
+	}
+}
---
+++ SpringLdapThymleaf/src/main/java/com/teamdev/app/web/HomeController.java
@@ -0,0 +1,29 @@
+package com.teamdev.app.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
+
+}
